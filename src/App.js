1dda6f315dbb5a7f537ae9d54a522a428a5149d7
Mattp15 <Matthewrphillips15@gmail.com>
2022-12-12 01:19:28 -0500

    syncing search results, don't think I need search by ID in primary function but might be nice to keep in

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ const accessToken = process.env.REACT_APP_ACCESS_TOKEN
 const App = () => {
   const [fetchResponse, setFetchResponse] = useState()
   const [imgState, setImgState] = useState()
+  const [itemId, setItemId] = useState()
+  const [itemName, setItemName] = useState("Primal Scion's Twinblade")
+  const [itemObject, setItemObject] = useState()
 
   const handleGetItem = async () => {
-    const response = await fetch('https://us.api.blizzard.com/data/wow/item/200293?namespace=static-us&locale=en_US&access_token=' + accessToken, {
+    const response = await fetch('https://us.api.blizzard.com/data/wow/item/' + itemId + '?namespace=static-us&locale=en_US&access_token=' + accessToken, {
       method: 'GET',
       headers: {
         'Content-Type': 'applicatoin/json;charset=UTF-8',
@@ -18,9 +21,35 @@ const App = () => {
     console.log(res)
   }
 
-  const getItemByName = async () => {}
+  const getItemByName = async () => {
+    const response = await fetch('https://us.api.blizzard.com/data/wow/search/item?namespace=static-us&name.en_US=Twinblade&orderby=id&_page=1&access_token=' + accessToken, {
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json;charset+UTF-8',
+      },
+    })
+    const res = await response.json()
+    const searchWord = itemName.split(' ').shift()
+    const dirtyObjectArray = res.results.map((x) => {
+      if (x.data.name.en_US === itemName) {
+        return x.data
+      }
+    })
+    const foundItemObject = dirtyObjectArray.filter((fil) => fil)
+    setItemObject(foundItemObject[0])
+    setItemId(foundItemObject[0].id)
+    const mediaResponse = await fetch('https://us.api.blizzard.com/data/wow/media/item/' + foundItemObject[0].id + '?namespace=static-10.0.2_46479-us&access_token=' + accessToken, {
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+    })
+    const mediaRes = await mediaResponse.json()
+    setImgState(mediaRes.assets[0].value)
+    console.log(mediaRes.assets[0].value)
+  }
   const getMedia = async () => {
-    const response = await fetch('https://us.api.blizzard.com/data/wow/media/item/200293?namespace=static-10.0.2_46479-us&access_token=' + accessToken, {
+    const response = await fetch('https://us.api.blizzard.com/data/wow/media/item/' + itemId + '?namespace=static-10.0.2_46479-us&access_token=' + accessToken, {
       method: 'GET',
       header: {
         'Content-Type': 'application/json;charset=UTF-8',
@@ -28,7 +57,6 @@ const App = () => {
     })
     const res = await response.json()
     setImgState(res.assets[0].value)
-    console.log(res.assets)
   }
   return (
     <div className='App'>
@@ -37,6 +65,7 @@ const App = () => {
         <button onClick={getItemByName}>get item by name</button>
         <button onClick={getMedia}>Get media</button>
         <Test state={imgState} />
+        <Test state={itemObject} />
       </header>
     </div>
   )
--- src/Components/temp.js
+++ src/Components/temp.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Test = ({ state }) => {
-  return (
-    <div>
-      <img src={state} alt='idk' />
-    </div>
-  )
+  useEffect(() => {
+    console.log(typeof state)
+  }, [])
+  if (typeof state == 'string') {
+    return (
+      <div>
+        <img src={state} alt='idk' />
+      </div>
+    )
+  }
+  if (typeof state === 'object') {
+    return (
+      <div>
+        <ul>
+          <li key='00'></li>
+          <li key='01'></li>
+          <li key='02'></li>
+          <li key='03'></li>
+          <li key='04'></li>
+          <li key='05'></li>
+          <li key='06'></li>
+        </ul>
+      </div>
+    )
+  }
 }
 export default Test
