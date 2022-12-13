529ae1eb23f2a2783ebc24dd68823d0d79c26ad3
Mattp15 <Matthewrphillips15@gmail.com>
2022-12-12 20:23:41 -0500

    figuring out data structure

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg'
 import './App.css'
 import React, { useState, useEffect } from 'react'
 import { Test } from './Components'
+import { getMouseEventOptions } from '@testing-library/user-event/dist/utils'
 const accessToken = process.env.REACT_APP_ACCESS_TOKEN
 const App = () => {
   const [fetchResponse, setFetchResponse] = useState()
   const [imgState, setImgState] = useState()
-  const [itemId, setItemId] = useState()
+  const [itemId, setItemId] = useState(200293)
   const [itemName, setItemName] = useState("Primal Scion's Twinblade")
   const [itemObject, setItemObject] = useState()
+  const [mousePos, setMousePos] = useState({})
 
   const handleGetItem = async () => {
     const response = await fetch('https://us.api.blizzard.com/data/wow/item/' + itemId + '?namespace=static-us&locale=en_US&access_token=' + accessToken, {
@@ -46,7 +48,6 @@ const App = () => {
     })
     const mediaRes = await mediaResponse.json()
     setImgState(mediaRes.assets[0].value)
-    console.log(mediaRes.assets[0].value)
   }
   const getMedia = async () => {
     const response = await fetch('https://us.api.blizzard.com/data/wow/media/item/' + itemId + '?namespace=static-10.0.2_46479-us&access_token=' + accessToken, {
@@ -58,6 +59,7 @@ const App = () => {
     const res = await response.json()
     setImgState(res.assets[0].value)
   }
+
   return (
     <div className='App'>
       <header className='App-header'>
--- src/Components/temp.js
+++ src/Components/temp.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 
 const Test = ({ state }) => {
   useEffect(() => {
-    console.log(typeof state)
+    console.log(state)
   }, [])
   if (typeof state == 'string') {
     return (
@@ -11,18 +11,24 @@ const Test = ({ state }) => {
       </div>
     )
   }
-  if (typeof state === 'object') {
+  // if (typeof state === 'object') {
+  //   return (
+  //     <div>
+  //       <ul>
+  //         <li key='00'></li>
+  //         <li key='01'></li>
+  //         <li key='02'></li>
+  //         <li key='03'></li>
+  //         <li key='04'></li>
+  //         <li key='05'></li>
+  //         <li key='06'></li>
+  //       </ul>
+  //     </div>
+  //   )
+  else {
     return (
       <div>
-        <ul>
-          <li key='00'></li>
-          <li key='01'></li>
-          <li key='02'></li>
-          <li key='03'></li>
-          <li key='04'></li>
-          <li key='05'></li>
-          <li key='06'></li>
-        </ul>
+        <p></p>
       </div>
     )
   }
