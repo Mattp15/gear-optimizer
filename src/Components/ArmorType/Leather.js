c3d1883adbd22f9d113995039dc1e5cca92b3810
Mattp15 <Matthewrphillips15@gmail.com>
2022-12-12 23:38:00 -0500

    Added leather raid loot to leather

--- src/Components/ArmorType/Leather.js
+++ src/Components/ArmorType/Leather.js
@@ -1,22 +1,22 @@
 import React, { useState } from 'react'
 
 const Leather = () => {
-  const [feet, setFeet] = useState([])
-  const [feetObjects, setFeetObjects] = useState([])
-  const [chest, setChest] = useState([])
+  const [chest, setChest] = useState(["Brawler's Earthen Curiass", "Valdrakken Protector's Turncoat"])
   const [chestObjects, setChestObjects] = useState([])
-  const [hands, setHands] = useState([])
+  const [feet, setFeet] = useState(['Galvanic Gaiters', "Ice-Climber's Cleats"])
+  const [feetObjects, setFeetObjects] = useState([])
+  const [hands, setHands] = useState(['Treacherous Totem Wraps'])
   const [handsObjects, setHandsObjects] = useState([])
-  const [head, setHead] = useState([])
+  const [head, setHead] = useState(['Windborne Hatsuburi'])
   const [headObjects, setHeadObjects] = useState([])
-  const [legs, setLegs] = useState([])
+  const [legs, setLegs] = useState(['Loathsome Thunderhosen'])
   const [legsObjects, setLegsObjects] = useState([])
-  const [waist, setWaist] = useState([])
-  const [waistObjects, setWaistObjects] = useState([])
-  const [wrist, setWrist] = useState([])
-  const [wristObjects, setWristObjects] = useState([])
-  const [shoulders, setShoulders] = useState([])
+  const [shoulders, setShoulders] = useState(['Twisted Loam Spaulders'])
   const [shouldersObjects, setShouldersObjects] = useState([])
+  const [waist, setWaist] = useState(['Fused Shale Waistband'])
+  const [waistObjects, setWaistObjects] = useState([])
+  const [wrist, setWrist] = useState(["Loyal Flametender's Bracers"])
+  const [wristObjects, setWristObjects] = useState([])
   return <h1> I am leather</h1>
 }
 export default Leather
