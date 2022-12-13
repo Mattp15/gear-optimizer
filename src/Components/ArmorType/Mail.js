0829abd4b4e367f197b9e2ea9284c2fcdae6384c
Mattp15 <Matthewrphillips15@gmail.com>
2022-12-12 23:40:10 -0500

    Added mail raid drops to mail

--- src/Components/ArmorType/Mail.js
+++ src/Components/ArmorType/Mail.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react'
 
 const Mail = () => {
-  const [chest, setChest] = useState([])
+  const [chest, setChest] = useState(["Embar's Ashen Hauberk"])
   const [chestObjects, setChestObjects] = useState([])
-  const [feet, setFeet] = useState([])
+  const [feet, setFeet] = useState(['Daring Chasm-Leapers'])
   const [feetObjects, setFeetObjects] = useState([])
-  const [hands, setHands] = useState([])
+  const [hands, setHands] = useState(['Calamitous Shockguards'])
   const [handsObjects, setHandsObjects] = useState([])
-  const [head, setHead] = useState([])
+  const [head, setHead] = useState(["Eranog's Adorned Sallet"])
   const [headObjects, setHeadObjects] = useState([])
-  const [legs, setLegs] = useState([])
+  const [legs, setLegs] = useState(['Tassets of the Tarasek Legion'])
   const [legsObjects, setLegsObjects] = useState([])
-  const [shoulders, setShoulders] = useState([])
+  const [shoulders, setShoulders] = useState(['Faultline Mantle', 'Frozen Claw Mantle'])
   const [shouldersObjects, setShouldersObjects] = useState([])
-  const [waist, setWaist] = useState([])
+  const [waist, setWaist] = useState(["Lavamancer's Ceremonial Waistguard", 'Unnatural Dripstone Cinch'])
   const [waistObjects, setWaistObjects] = useState([])
-  const [wrist, setWrist] = useState([])
+  const [wrist, setWrist] = useState(['Surging-Song Conductors'])
   const [wristObjects, setWristObjects] = useState([])
   return <h1>I am Mail</h1>
 }
